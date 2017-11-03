1e1ae171c01375090d35c3d3a057f3c6fe909063
Fernando Lopez Martinez <fer.lopez94@outlook.com>
2017-11-03 14:01:12 -0600

    Add images to family and known categories in Contacts view

--- MisRecuerdos/MisRecuerdos/ContactsViewController.swift
+++ MisRecuerdos/MisRecuerdos/ContactsViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 class ContactsViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var familyPhoto: UIImageView!
+    @IBOutlet weak var knownPhoto: UIImageView!
+    
+    
     // MARK: - Instance variables
     
     var user: User? = nil
+    var familyContacts = [Contact]()
+    var knownContacts = [Contact]()
+    
     let segueToShowAll = "segueToShowAll"
     let segueToShowFamily = "segueToShowFamily"
     let segueToShowKnown = "segueToShowKnown"
-
+    
+    var timer = Timer()
+    var imageIndex = 0
+    
     
     // MARK: - View Controller life cycle
     
@@ -31,8 +43,37 @@ class ContactsViewController: UIViewController {
         self.user = user
         print(self.user!)
         print(self.user!.contacts)
+        familyContacts = self.user!.contacts.filter {$0.category == .family }
+        knownContacts = self.user!.contacts.filter {$0.category == .known }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        imageIndex = 0
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImages), userInfo: nil, repeats: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func changeImages() {        
+        if familyContacts.count > 0 {
+            let contact = familyContacts[imageIndex % familyContacts.count]
+            familyPhoto.image = contact.photo
+        }
+        
+        if knownContacts.count > 0 {
+            let contact = knownContacts[imageIndex % knownContacts.count]
+            knownPhoto.image = contact.photo
+        }
+        
+        imageIndex += 1
+    }
+    
+    
+    // MARK: - Navigation
     
     @IBAction func unwindToContacts(segue: UIStoryboardSegue) {}
     
@@ -45,22 +86,20 @@ class ContactsViewController: UIViewController {
         self.user = user
         print(self.user!)
         print(self.user!.contacts)
+        familyContacts = self.user!.contacts.filter {$0.category == .family }
+        knownContacts = self.user!.contacts.filter {$0.category == .known }
     }
     
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == segueToShowAll {
             let vc = segue.destination as! ContactsTableViewController
             vc.contacts = user!.contacts
         } else if segue.identifier == segueToShowFamily {
             let vc = segue.destination as! ContactsTableViewController
-            vc.contacts = user!.contacts.filter {$0.category == .family }
+            vc.contacts = familyContacts
         } else if segue.identifier == segueToShowKnown {
             let vc = segue.destination as! ContactsTableViewController
-            vc.contacts = user!.contacts.filter {$0.category == .known }
+            vc.contacts = knownContacts
         }
     }
 
